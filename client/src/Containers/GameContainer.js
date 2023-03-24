78cf8ccdaf7d4b5736287adc7d73d630ef836cd8
Michael Murray <mmurray24@hotmail.com>
2023-03-24 15:17:09 +0000

    added shuffle function

--- client/src/Containers/GameContainer.js
+++ client/src/Containers/GameContainer.js
@@ -10,10 +10,19 @@ const GameContainer = () => {
     const [cpu,SetCPU] = useState([]);
     const [middle,SetMiddle] = useState([]);
 
+    const shuffle = (cards) => {
+        for (let i = cards.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [cards[i], cards[j]] = [cards[j], cards[i]];
+        }
+        return cards;
+    };
+
     useEffect(() => {
         getDinosaurs()
         .then((data) => {
             const playerHand = data
+            shuffle(playerHand)
             const cpuHand = playerHand.splice(0,15)
             SetPlayer(playerHand)
             SetCPU(cpuHand)
@@ -22,13 +31,13 @@ const GameContainer = () => {
     },[]);
 
 
-      return (
+    return (
         <>
-        <p> Game Container! </p>
-        <PlayerCard/>
-        <ComputerCard/>
+            <p> Game Container! </p>
+            <PlayerCard player={player}/>
+            <ComputerCard cpu={cpu}/>
         </>
-    )
+    );
 };
 
 export default GameContainer; 
