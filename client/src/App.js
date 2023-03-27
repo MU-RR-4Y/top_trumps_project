6781caf5c976385ab5fd28a5040e32795995795c
SShingler <s.shingler@hotmail.co.uk>
2023-03-27 15:10:50 +0100

    add theme audio

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import GameContainer from './Containers/GameContainer';
 function App() {
   return (
     <div className="App">
-      <h1>test</h1>
+      <h1>Dino-Trumps!</h1>
       <GameContainer/>
     </div>
   );
