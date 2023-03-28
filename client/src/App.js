93fe9c76ec528d94186ff707267f32fb00bcc2f2
LaurenLingham <lauren.lingham@outlook.com>
2023-03-28 10:47:36 +0100

    styling for draw pile

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div className='Title'>
-      <h1>Dino-Trumps!</h1>
+        <h1>Dino-Trumps!</h1>
       </div>
       <GameContainer/>
     </div>
